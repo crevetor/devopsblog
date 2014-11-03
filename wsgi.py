acd1863462ffc9da5eb30038e1554f744aa28d60
Antoine Reversat <a.reversat@gmail.com>
2014-11-03 16:42:16 -0500

    Fix wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devopsblog.devopsblog.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devopsblog.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
