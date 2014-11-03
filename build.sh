6a980dbdea66aa335bab6426209db7ede3460850
Antoine Reversat <a.reversat@gmail.com>
2014-11-03 15:27:34 -0500

    Add a build script

---
+++ build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]
+then
+    echo "$0 <type> <workspace>"
+    echo "type: either jenkins, dev or stage"
+    echo "workspace: the directory where the app lies"
+    echo "if type is dev we assume that you have already activated your venv"
+    exit 1
+fi
+
+TYPE=$1
+WORKSPACE=$2
+REQ="requirements.txt"
+
+if [ "$TYPE" != "jenkins" -a "$TYPE" != "stage" -a "$TYPE" != "dev" ]
+then
+    echo "Type must be either jenkins, dev or stage"
+    exit 1
+fi
+
+if [ "$TYPE" = "jenkins" -o "$TYPE" = "dev" ]
+then
+    REQ="dev-requirements.txt"
+fi
+
+if [ "$TYPE" != "dev" ]
+then
+    source $WORKSPACE/../env/bin/activate
+fi
+cd $WORKSPACE
+
+pip install -r $REQ
+python devopsblog/manage.py syncdb --noinput
+python devopsblog/manage.py migrate
+python devopsblog/manage.py collectstatic --noinput
+
+if [ "$TYPE" = "jenkins" ]
+then
+    python manage.py jenkins
+fi
+
+touch $WORKSPACE/reload-uwsgi
